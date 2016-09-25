b76aa257b1565cb66d1aaf5796f8312991f1cb4b
Marina123456 <marjnazhjrnova@yandex.ru>
2016-09-25 17:34:22 +0300

    изменения

--- sendSMS.php
+++ sendSMS.php
@@ -28,9 +28,10 @@ function sendSMS(){
     $telephone='%2B'.$telephone;
 
     $text="Rate ".$_GET["namerate"]." - ".$_GET["rate"]." RUB";
+
     $convertedText = mb_convert_encoding($text, 'utf-8', mb_detect_encoding($text));
 
-    $send_url='https://api.iqsms.ru/messages/v2/send/?login='.$login.'&password='.$password.'&phone='.$telephone.'&text='.$convertedText;
+    $send_url='http://api.iqsms.ru/messages/v2/send/?login='.$login.'&password='.$password.'&phone='.$telephone.'&text='.$convertedText;
 
     $send=file_get_contents($send_url);
     echo $send."<br>";
